ce16ebdb5e4b0eb956edbf22688b30a3354643ad
karthik r <karthik.r@synconext.com>
2021-10-11 07:23:52 +0530

    react-create-app on heroku

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import logo from './logo.svg';
+import React,{useReducer} from 'react'
 import './App.css';
-
+import {v4 as uuidv4 } from 'uuid';
+import { Button } from 'react-bootstrap';
+import ToDoList from './ToDoList';
+const todosInitialState = {
+  todos:[]
+}
+function todosReducer(state,action)
+{
+  switch(action.type)
+  {
+    case 'get':
+      return {...state,todos:action.payload}
+    case 'add':
+      const addedToDos = [...state.todos,action.payload];
+      return {...state,todos:addedToDos};
+    case 'delete':
+      const todofilterlist=state.todos.filter(todo=>todo.id!==action.payload.id);
+      console.log('hello')
+      return {...state,todos:todofilterlist};
+    case 'edit':
+      const updateToDo = {...action.payload};
+      const updatedToDoIndex = state.todos.findIndex(t=> t.id===action.payload.id);
+      const updatedToDos = [
+        ...state.todos.slice(0,updatedToDoIndex),
+        updateToDo,
+        ...state.todos.slice(updatedToDoIndex+1)
+      ];
+      return {...state,todos:updatedToDos}
+    default:
+      return todosInitialState;
+  }
+}
+export const TodosContext = React.createContext();
 function App() {
+  const [state,dispatch] =  useReducer(todosReducer,todosInitialState)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <TodosContext.Provider value={{state,dispatch}}>
+     <ToDoList/>
+
+     </TodosContext.Provider>
   );
 }
 
---
+++ src/ToDoList.js
@@ -0,0 +1,85 @@
+import React,{useContext,useEffect,useState} from "react";
+import { TodosContext } from "./App";
+import {Table,Form,Button} from 'react-bootstrap'
+import useAPI from "./useApi";
+import axios from "axios";
+import {v4 as uuidv4 } from 'uuid';
+function ToDoList()
+{
+    const {state,dispatch} = useContext(TodosContext)
+    const [toDoText,setTodoText] =useState("")
+    const [editMode, setEditMode] =useState(false)
+    const [editToDo, setEditToDo] =useState(null)
+    const buttonTitle = editMode ? 'Edit':'Add';
+
+    const endpoint = "http://localhost:3000/todos/"
+    const savedTodos = useAPI(endpoint)
+    useEffect(()=>{
+        dispatch({type:"get",payload:savedTodos})},[savedTodos])
+    const handleSubmit = async event=>{
+        event.preventDefault();
+        if(editMode)
+        {
+            await axios.patch(endpoint+editToDo.id,{text:toDoText})
+            dispatch({type:'edit',payload:{...editToDo,text:toDoText}})
+            setEditMode(false);
+            setEditToDo(null);
+        }
+        else
+        {
+            const newToDo = {id:uuidv4(),text:toDoText}
+            const response = await axios.post(endpoint,newToDo)
+            dispatch({type:'add',payload:newToDo})
+        }
+        setTodoText("")
+    };
+    return(
+        <div>
+        <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Control
+                 type="text" 
+                 placeholder="Enter ToDo"
+                 onChange={event=>setTodoText(event.target.value)}
+                 value={toDoText}
+                 />
+                </Form.Group>
+                <Button variant="primary" type="submit">{buttonTitle}</Button>
+            </Form>
+        <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>To Do</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+         {
+             state.todos.map(todo=>(
+                 <tr key={todo.id}>
+                     <td>{todo.text}</td>
+
+                     <td onClick={()=>{
+                         setTodoText(todo.text);
+                         setEditMode(true)
+                         setEditToDo(todo)
+                     }}>
+                        <Button variant="link">Edit</Button>
+                     </td>
+
+                     <td onClick={ async ()=> { 
+                         await axios.delete(endpoint + todo.id)
+                         dispatch({type:'delete',payload:todo})
+                         }}>
+                             <Button variant="link">Delete</Button>
+                         </td>
+                     </tr>
+             ))
+         }
+        </tbody>
+      </Table>
+      </div>
+    )
+}
+export default ToDoList;
